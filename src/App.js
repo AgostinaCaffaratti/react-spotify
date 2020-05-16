8131dcb08add600106bdf3ce92c989eafadd5a50
Agostina Caffaratti <agostina.caffaratti@globant.com>
2020-05-16 16:55:24 -0300

    comienzo de html y estilado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.scss'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='container-header' >
+        <img className='container-header__logo' 
+        src= {process.env.PUBLIC_URL + "/assets/logo.png"} 
+        alt='logo'>
+
+        </img>
+        <img className='container-header__loop'
+        src= {process.env.PUBLIC_URL + "/assets/loop.png"} 
+        alt='logo'>
+
+        </img>
+        <form className='container-header__search'>
+          <input type='search' 
+          className='container-header__search--input' 
+          placeholder='Search for another Artist'>
+          </input>
+        </form>
       </header>
+      <section className='container-welcome'>
+        <h3 className='container-welcome__title'>Welcome to</h3>
+        <h1 className='container-welcome__h1'>SpotiSearch</h1>
+        <p className='container-welcome__text'>Search your favourite songs over Spotify, 
+          just enter an artistist's name in the following search box and enjoy!
+        </p>
+        <form className='container-welcome__title'>
+          <input type='search' 
+          className='container-welcome__title--input' 
+          placeholder='Search for another Artist'>
+          </input>
+        </form>
+
+      </section>
+      
+     
+      
+
+     
     </div>
   );
 }
